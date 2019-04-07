c8e0a130aeeac28cd7ac42f8eb681f90f4c57d10
TheAner <theaner123@gmail.com>
2019-04-07 15:55:39 +0200

    Modified getDefaultRoles to compatible with Java 8

--- src/main/java/gg/gamello/user/dao/type/RoleType.java
+++ src/main/java/gg/gamello/user/dao/type/RoleType.java
@@ -2,6 +2,7 @@ package gg.gamello.user.dao.type;
 
 import gg.gamello.user.dao.Role;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public enum RoleType {
@@ -14,7 +15,12 @@ public enum RoleType {
         this.privilegeLevel = privilegeLevel;
     }
 
+// Java 1.9
+//    public static Set<Role> getDefaultRoles() {
+//        return Set.of(new Role(RoleType.USER));
+//    }
+
     public static Set<Role> getDefaultRoles() {
-        return Set.of(new Role(RoleType.USER));
+        return new HashSet<Role>() {{ add(new Role(RoleType.USER)); }};
     }
 }
