4b645c7d6915f29cb66c4fd386911aeec930f65a
Adrian Sowiński <theaner123@gmail.com>
2019-09-21 13:34:25 +0200

    added email sending switch

--- src/main/java/gg/gamello/user/provider/EmailProvider.java
+++ src/main/java/gg/gamello/user/provider/EmailProvider.java
@@ -2,6 +2,7 @@ package gg.gamello.user.provider;
 
 import gg.gamello.user.domain.EmailRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +10,15 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class EmailProvider {
 
+    @Value("${email.enabled:true}")
+    private boolean emailEnabled;
+
     @Autowired
     private RestTemplate restTemplate;
 
     public ResponseEntity<String> sendEmail(EmailRequest emailRequest) {
-        return restTemplate.postForEntity("http://email/api/transactional/single", emailRequest, String.class);
+        if (emailEnabled)
+            return restTemplate.postForEntity("http://email/api/transactional/single", emailRequest, String.class);
+        return ResponseEntity.ok(emailRequest.toString());
     }
 }
