e27c5527629aec3b4a66ea49b4c0d0f6d31e4c71
Adrian Sowiński <theaner123@gmail.com>
2019-11-05 16:18:59 +0100

    specified default routes security

--- src/main/java/gg/gamello/user/core/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/gg/gamello/user/core/infrastructure/config/SecurityConfiguration.java
@@ -7,11 +7,14 @@ import gg.gamello.dev.authentication.security.JwtAuthenticationFilter;
 import gg.gamello.dev.authentication.security.JwtAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
@@ -28,16 +31,41 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		this.objectMapper = objectMapper;
 		this.authProperties = authProperties;
 	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.anyRequest().authenticated();
+		http
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/")
+				.permitAll()
+				.antMatchers( "/confirm/**", "/recover", "/validate/**", "/api/**", "/id/**", "/special/**")
+				.permitAll()
+				.antMatchers("/", "/change/**")
+				.authenticated();
+
+		http
+				.formLogin()
+				.disable()
+				.logout()
+				.disable();
 
 		http.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 		http.csrf().disable();
+	}
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring()
+				.antMatchers(HttpMethod.POST, "/")
+				.antMatchers( "/confirm/**", "/recover", "/validate/**", "/api/**", "/id/**", "/special/**");
 	}
 
 	@Override
