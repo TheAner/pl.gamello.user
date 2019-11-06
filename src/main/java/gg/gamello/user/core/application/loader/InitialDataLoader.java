38b045e6400e3f1c0e30161607af6e99cae4a35c
Adrian Sowiński <theaner123@gmail.com>
2019-11-06 22:49:14 +0100

    added initial data loader for roles

---
+++ src/main/java/gg/gamello/user/core/application/loader/InitialDataLoader.java
@@ -0,0 +1,37 @@
+package gg.gamello.user.core.application.loader;
+
+import gg.gamello.user.core.domain.role.RoleFactory;
+import gg.gamello.user.core.domain.role.RoleRepository;
+import gg.gamello.user.core.domain.role.RoleType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Stream;
+
+@Slf4j
+@Component
+public class InitialDataLoader implements ApplicationListener<ContextRefreshedEvent> {
+
+	private RoleRepository roleRepository;
+
+	private RoleFactory roleFactory;
+
+	public InitialDataLoader(RoleRepository roleRepository, RoleFactory roleFactory) {
+		this.roleRepository = roleRepository;
+		this.roleFactory = roleFactory;
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		Stream.of(RoleType.values()).forEach(this::createRoleIfNotFound);
+	}
+
+	@Transactional
+	void createRoleIfNotFound(RoleType roleType) {
+		if(!roleRepository.existsByRole(roleType))
+			roleRepository.save(roleFactory.create(roleType));
+	}
+}
