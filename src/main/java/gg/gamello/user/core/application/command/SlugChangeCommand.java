ed2bfb8915b8d84f0fa2dafa0d8ce80b5f5b9442
Adrian Sowiński <theaner123@gmail.com>
2019-11-12 11:42:09 +0100

    added size validation for slug

--- src/main/java/gg/gamello/user/core/application/command/SlugChangeCommand.java
+++ src/main/java/gg/gamello/user/core/application/command/SlugChangeCommand.java
@@ -3,9 +3,11 @@ package gg.gamello.user.core.application.command;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class SlugChangeCommand {
 	@NotEmpty(message = "Slug can not be empty")
+	@Size(min = 3, max = 24, message = "Slug must be between {min} and {max} characters long")
 	String slug;
 }
