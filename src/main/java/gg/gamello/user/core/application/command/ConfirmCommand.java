52a4a7b687ac907f4192ae024457135ab565d3a3
Adrian Sowiński <theaner123@gmail.com>
2019-11-12 16:46:32 +0100

    refactored @NotEmpty for UUID to @NotNull

--- src/main/java/gg/gamello/user/core/application/command/ConfirmCommand.java
+++ src/main/java/gg/gamello/user/core/application/command/ConfirmCommand.java
@@ -3,11 +3,12 @@ package gg.gamello.user.core.application.command;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
 public class ConfirmCommand {
-	@NotEmpty(message = "UserId can not be empty")
+	@NotNull(message = "UserId can not be null")
 	UUID userId;
 	@NotEmpty(message = "Secret can not be empty")
 	String secret;
