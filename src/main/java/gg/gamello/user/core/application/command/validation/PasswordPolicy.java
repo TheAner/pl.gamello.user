bc7cc6b26e490e70365239fe5c88880b61d262c2
Adrian Sowiński <theaner123@gmail.com>
2019-11-12 12:41:14 +0100

    refactored password policy validator

--- src/main/java/gg/gamello/user/core/application/command/validation/PasswordPolicy.java
+++ src/main/java/gg/gamello/user/core/application/command/validation/PasswordPolicy.java
@@ -2,20 +2,19 @@ package gg.gamello.user.core.application.command.validation;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import javax.validation.constraints.Pattern;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target(ElementType.FIELD)
-@Constraint(validatedBy={})
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+@Constraint(validatedBy = PasswordPolicyValidator.class)
 @Retention(RUNTIME)
-@Pattern(regexp="((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{8,64})")
 public @interface PasswordPolicy {
 	String message() default "Password must be 8 to 64 long, contains upper, lower case characters," +
 			" one numeric and one special character";
+	String regexp() default "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{8,64})";
 	Class<?>[] groups() default {};
 	Class<? extends Payload>[] payload() default {};
 }
---
+++ src/main/java/gg/gamello/user/core/application/command/validation/PasswordPolicyValidator.java
@@ -0,0 +1,25 @@
+package gg.gamello.user.core.application.command.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordPolicyValidator implements ConstraintValidator<PasswordPolicy, String> {
+
+	private String message;
+	private String regexp;
+
+	@Override
+	public void initialize(PasswordPolicy constraintAnnotation) {
+		message = constraintAnnotation.message();
+		regexp = constraintAnnotation.regexp();
+	}
+
+	@Override
+	public boolean isValid(String password, ConstraintValidatorContext context) {
+		if (password.matches(regexp))
+			return true;
+		context.disableDefaultConstraintViolation();
+		context.buildConstraintViolationWithTemplate(message).addConstraintViolation();
+		return false;
+	}
+}
